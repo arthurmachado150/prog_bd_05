abca1822d45624b3c33808c7c8c8b3b9cacb18e0
Arthur Furtado Machado <arthur_machado150@estudante.sesisenai.org.br>
2026-05-15 16:20:52 -0300

    Versão final

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize('db_veiculo', 'root', 'senai', {
+const db = new Sequelize('db_veiculo_02', 'root', 'senai', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
